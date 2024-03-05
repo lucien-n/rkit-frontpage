3b0a3025de622e0a2eaf4267ef1942f760a1c522
Lucien <lucien.neuhoff@protonmail.com>
2024-03-05 21:42:43 +0100

    adding background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+	class="dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black"
+>
+	<div
+		class="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"
+	></div>
+	<p
+		class="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl"
+	>
+		Backgrounds
+	</p>
+</div>
