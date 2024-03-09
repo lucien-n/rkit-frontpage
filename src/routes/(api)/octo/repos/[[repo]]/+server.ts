f99c97d132fe1d22a1317f17b1c3656e376c3ad5
Lucien <lucien.neuhoff@protonmail.com>
2024-03-09 16:07:27 +0100

    fix: repo fetching

--- src/routes/(api)/octo/repos/[[repo]]/+server.ts
+++ src/routes/(api)/octo/repos/[[repo]]/+server.ts
@@ -4,16 +4,15 @@ import { json, type RequestHandler } from '@sveltejs/kit';
 export const GET: RequestHandler = async (event) => {
 	const { repo } = event.params;
 
-	const query = 'q=' + encodeURIComponent(`repo:${owner}/${repo || ''}`);
 	const { data } = await octokit.request('GET /search/repositories', {
-		q: query,
+		q: `owner:${owner} repo:${repo}`,
 		sort: 'updated',
+		order: 'desc',
 		per_page: 10
 	});
 	const repositories = data.items
 		.map(({ name }) => name)
 		.filter((name) => new RegExp(`${repo || ''}`, 'i').test(name));
 
-	console.log(data, repositories);
 	return json(repositories);
 };
