21b63e03158e554e24fc6a7cef450918e5266cd9
Lucien <lucien.neuhoff@protonmail.com>
2024-03-09 18:46:57 +0100

    feat: project card skeleton and fixed col span

---
+++ src/lib/components/project-card-skeleton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import * as Card from '$shadcn/card';
+	import { Skeleton } from '$shadcn/skeleton';
+</script>
+
+<Card.Root class="relative h-full w-full">
+	<Card.Header class="text-3xl font-bold">
+		<Skeleton class="h-7 w-48" />
+	</Card.Header>
+	<Card.Content class="flex flex-col gap-3">
+		<Skeleton class="h-4 w-72" />
+		<Skeleton class="h-4 w-36" />
+	</Card.Content>
+</Card.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,41 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+	import ProjectCardSkeleton from '$lib/components/project-card-skeleton.svelte';
 	import ProjectCard from '$lib/components/project-card.svelte';
 	import { cn } from '$lib/shadcn/utils';
-	import type { Project } from '$shared/modules/projects/project.entity';
-	import { ProjectsController } from '$shared/modules/projects/projects.controller';
-	import { onMount } from 'svelte';
+	import { remlive } from '$lib/utils/remlive';
+	import { Project } from '$shared/modules/projects/project.entity';
+	import { remult } from 'remult';
 
-	let getProjects: Promise<Project[] | undefined>;
+	let projects = remlive(remult.repo(Project), []);
 
-	onMount(() => {
-		getProjects = ProjectsController.find({ where: { hidden: false } });
-	});
+	$: browser &&
+		projects.listen({
+			orderBy: { createdAt: 'asc' },
+			include: { author: true },
+			where: { hidden: false },
+			limit: 25
+		});
+
+	const getColSpan = (index: number) =>
+		['col-span-1', 'col-span-2', 'col-span-2', 'col-span-1'][index % 4];
 </script>
 
 <div class="flex h-full w-full items-center justify-center">
 	<div class="grid h-2/3 w-2/3 grid-cols-7 grid-rows-3 gap-5">
-		{#await getProjects}
-			<p>fetching projects</p>
-		{:then projects}
-			{#each projects ?? [] as project, index (project.id)}
-				<article class={cn('h-full', index % 3 == 0 ? 'col-span-4' : 'col-span-3')}>
+		{#if $projects.length}
+			{#each $projects as project, index (project.id)}
+				<article class={cn('h-full', getColSpan(index))}>
 					<ProjectCard {project} />
 				</article>
 			{/each}
-		{/await}
+		{:else}
+			<!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
+			{#each { length: 6 } as _, index}
+				<article class={cn('h-full', getColSpan(index))}>
+					<ProjectCardSkeleton />
+				</article>
+			{/each}
+		{/if}
 	</div>
 </div>
