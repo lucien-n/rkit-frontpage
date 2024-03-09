98a2032f30b0b2f8d3edc25f61b934790ea2b17e
Lucien <lucien.neuhoff@protonmail.com>
2024-03-09 16:37:01 +0100

    optional setProjectScehma

--- src/shared/modules/projects/schemas/set-project.schema.ts
+++ src/shared/modules/projects/schemas/set-project.schema.ts
@@ -5,10 +5,10 @@ import projectRules from '../project.rules';
 export const setProjectSchema = z
 	.object({
 		id: z.string().cuid().optional(),
-		name: getZString('name', projectRules.field.name).default(''),
-		description: getZString('description', projectRules.field.description).default(''),
-		repo: getZString('repo', projectRules.field.repo).default(''),
-		branch: getZString('branch', projectRules.field.branch).default(''),
+		name: getZString('name', projectRules.field.name).default('').optional(),
+		description: getZString('description', projectRules.field.description).default('').optional(),
+		repo: getZString('repo', projectRules.field.repo).default('').optional(),
+		branch: getZString('branch', projectRules.field.branch).default('').optional(),
 		hidden: z.boolean().default(false)
 	})
 	.refine(
