306e84e60b73c0a1dd09b3be09f836b5535e653f
Lucien <lucien.neuhoff@protonmail.com>
2024-03-09 16:10:28 +0100

    ref: default values on setProjectSchema

--- src/shared/modules/projects/schemas/set-project.schema.ts
+++ src/shared/modules/projects/schemas/set-project.schema.ts
@@ -5,15 +5,16 @@ import projectRules from '../project.rules';
 export const setProjectSchema = z
 	.object({
 		id: z.string().cuid().optional(),
-		name: getZString('name', projectRules.field.name).optional(),
-		description: getZString('description', projectRules.field.description).optional(),
-		repo: getZString('repo', projectRules.field.repo).optional(),
-		branch: getZString('branch', projectRules.field.branch).optional(),
+		name: getZString('name', projectRules.field.name).default('').optional(),
+		description: getZString('description', projectRules.field.description).default('').optional(),
+		repo: getZString('repo', projectRules.field.repo).default('').optional(),
+		branch: getZString('branch', projectRules.field.branch).default('').optional(),
 		hidden: z.boolean().default(false).optional()
 	})
 	.refine(
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		({ id, ...rest }) => id || Object.entries(rest).every(([_key, value]) => value !== undefined)
+		({ id, ...rest }) =>
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			id || Object.entries(rest).every(([_key, value]) => ![undefined, ''].includes(value as never))
 	);
 
 export type SetProjectSchema = typeof setProjectSchema;
