d01848bea9cd10586edb696b21040f73c97b9c90
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-03-15 20:05:20 +0100

    feat: populate db with projects

--- src/lib/server/remult.ts
+++ src/lib/server/remult.ts
@@ -2,6 +2,8 @@ import { remultSveltekit } from 'remult/remult-sveltekit';
 import { createPostgresDataProvider } from 'remult/postgres';
 import { POSTGRES_URL } from '$env/static/private';
 import { controllers, entities } from '$shared';
+import { Project } from '$shared/modules/projects/project.entity';
+import data from './data.json';
 
 export const _api = remultSveltekit({
 	dataProvider: createPostgresDataProvider({ connectionString: POSTGRES_URL }),
@@ -19,5 +21,12 @@ export const _api = remultSveltekit({
 		}
 
 		return undefined;
+	},
+	initApi: async (remult) => {
+		if ((await remult.repo(Project).find()).length < 1) {
+			for (const project of data.projects) {
+				await remult.repo(Project).insert(project);
+			}
+		}
 	}
 });
