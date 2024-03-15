c83e6a1270c15e68b447fb3bf1c9452f7431caeb
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-03-15 20:22:46 +0100

    export tag

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,6 +2,8 @@ import { AuthController } from './modules/auth/auth.controller';
 import { Session } from './modules/auth/session.entity';
 import { Project } from './modules/projects/project.entity';
 import { ProjectsController } from './modules/projects/projects.controller';
+import { Tag } from './modules/tags/tag.entity';
+import { TagsController } from './modules/tags/tags.controller';
 
-export const entities = [Session, Project];
-export const controllers = [AuthController, ProjectsController];
+export const entities = [Session, Project, Tag];
+export const controllers = [AuthController, ProjectsController, TagsController];
